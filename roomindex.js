0f02ce4f332a28a53d767879f65e7c2a9e4970a7
soniamitav <soniamitav@gmail.com>
2018-09-25 02:56:58 +0530

    Added functionality to Prefferences Input

--- roomindex.js
+++ roomindex.js
@@ -15,3 +15,12 @@ function logout(){
   firebase.auth().signOut();
   window.open("login.html","_self");
 }
+
+function uncheck(){
+  document.getElementById("overrideflag").checked = false;
+}
+
+function pChanger(){
+  var state = document.getElementById("overrideflag");
+    document.getElementById("pOverridden").innerHTML = (state.checked)? "Anyone":"Preferred Roommate";
+}
